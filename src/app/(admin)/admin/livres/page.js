7ca0347ab2d3669d5b25c30828ba09bc4d2f2329
sh <sandrahammamitlili@gmail.com>
2024-01-13 14:28:25 +0100

    db4free-commit

--- src/app/(admin)/admin/livres/page.js
+++ src/app/(admin)/admin/livres/page.js
@@ -10,14 +10,14 @@ const ListLivres = dynamic(() => import('@/components/admin/listLivres'), {
 
 const getBooks=async()=>{ 
 const response = await fetch(process.env.URL +"/api/livres", { cache: 'no-store' });
-console.log(response)
+
 const data = await response.json();
 
 return data;
 }
 const ProductPage = async() =>{
     const livres=await getBooks()
-    
+    console.log(livres)
   return (
    <div className="container">
       <ListLivres livres={livres}/>  
